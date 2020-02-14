309455cfb2d0fc551e8de714b0216ac8de7e69c3
Maurício Dessuy <mauriciodessuy@hotmail.com>
2020-02-14 15:25:52 -0300

    learned about scrollview

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.body}>
+      <View style={styles.header}>
+        
+      </View>
+      <ScrollView>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+        <View style={styles.quadrado2}></View>
+        <View style={styles.quadrado1}></View>
+      </ScrollView>
+      <View style={styles.header}>
+        
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  body: {
+    paddingTop: 20,
+    flex: 1
   },
+  quadrado1: {
+    backgroundColor: '#ff0000',
+    height: 200
+  }, 
+  quadrado2: {
+    height: 100,
+    backgroundColor: '#00ff00'
+  },
+  header: {
+    backgroundColor: '#0000ff',
+    height: 100
+  }
 });
