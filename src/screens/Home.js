30b49f739a0882d7ff3c66bf4e5434540965b1eb
Maurício Dessuy <mauriciodessuy@hotmail.com>
2020-03-01 16:49:22 -0300

    learned about stack options

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -10,11 +11,23 @@ const Stack = createStackNavigator();
 export default function Home() {
   return (
       <Stack.Navigator initialRouteName="Feed" 
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="Feed" component={Feed} options={{ title: 'Feed' }}/>
-        <Stack.Screen name="Perfil" component={Perfil} />
+        headerMode='float'
+         >
+        <Stack.Screen name="Feed" component={Feed} options={{ 
+          title: 'Feed',
+        }}/>
+        <Stack.Screen name="Perfil" component={Perfil} 
+          options={{
+              headerBackImage: () => (
+                <View style={{width: 30, height: 30, backgroundColor: 'red'}}>
+  
+                </View>
+              ),
+              headerRight: () => (
+                <View style={{width: 30, height: 30, backgroundColor: 'green', marginRight: 20}}></View>
+              )
+          }}
+          />
       </Stack.Navigator>
   );
 }
